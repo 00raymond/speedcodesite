4bbca01165546ca3c3c785a4332554d14766f5e2
Raymond House <raymondhouse2003@gmail.com>
2024-04-15 00:18:16 -0700

    other files

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Assessment from './eval/Assessment';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Assessment />
+      </header>
+    </div>
+  );
+}
+
+export default App;
